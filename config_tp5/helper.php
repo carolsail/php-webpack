4d967845017c9e98b9ad84c87833842f503537e4
carolsail <1032217152@qq.com>
2019-11-28 10:15:29 +0800

    Create helper.php

---
+++ config_tp5/helper.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!function_exists('build_assets')) {
+    /**
+     * 遍历webpack.php中css | js
+     * @param string $entry 入口名
+     * @param string $type 资源类型
+     * @return string
+     */
+    function build_assets($entry, $type='js')
+    {
+        $back = '';
+        $map = config('webpack.');
+        $assets = $map[$type];
+        if (isset($assets[$entry])) {
+        	foreach ($assets[$entry] as $v) {
+                if (strpos($v, 'http')===false) {
+                    $v = rtrim(url("/", '', false), '/') . '/static' . $v;
+                }
+        		$back .= $type === 'js' ? '<script src="' . $v . '"></script>' : '<link rel="stylesheet" href="' . $v . '">';
+        	}
+        }
+        return $back;
+    }
+}
